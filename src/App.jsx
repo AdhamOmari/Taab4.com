e0fc9ee868bad0000b1a31e48f9128c3cf8d3fb0
adhamOmari <adhamomarics@gmail.com>
2024-07-20 01:14:38 +0300

    add the deales and fix the table

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div  className="back_ground">
+      <div className="text_container">
+        <h1>This website is under construction until August'5 but...</h1>
+        <h2>We'll be back very soon</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
